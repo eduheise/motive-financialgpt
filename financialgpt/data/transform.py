8d3e38a3ced8fd87fd84926a911107c3d219985e
Eduardo Heise <eduardo.heise@andela.com>
2024-07-15 10:57:04 -0300

    changing transform function order
    Signed-off-by: Eduardo Heise <eduardo.heise@andela.com>

--- financialgpt/data/transform.py
+++ financialgpt/data/transform.py
@@ -3,6 +3,77 @@ import pandas as pd
 from pandas import DataFrame
 
 
+def normalize_target_allocation(
+    target_allocation: DataFrame,
+) -> tuple[DataFrame, DataFrame]:
+    """
+    Create target allocation and client profile DataFrames from the target allocation DataFrame.
+
+    Args:
+        target_allocation (DataFrame): The target allocation DataFrame.
+
+    Returns:
+        tuple[DataFrame, DataFrame]: A tuple containing the target allocation DataFrame and the client profile DataFrame.
+    """
+    target_allocation = target_allocation.iloc[:200]
+    client_profile = (
+        target_allocation.groupby(by=["Client", "Target Portfolio"])
+        .count()
+        .reset_index()[["Client", "Target Portfolio"]]
+    )
+
+    target_allocation = target_allocation.drop("Target Portfolio", axis=1)
+    target_allocation["Client"] = [f"Client_{int(i / 4) + 1}" for i in range(50 * 4)]
+    target_allocation["Asset Class"] = ["Stocks", "Bonds", "ETFs", "Cash"] * 50
+
+    allocation_fill_df = (
+        target_allocation.groupby(by="Client")
+        .sum()
+        .reset_index()[["Client", "Target Allocation (%)"]]
+    )
+    replacements = dict(
+        zip(
+            allocation_fill_df["Client"],
+            100 - allocation_fill_df["Target Allocation (%)"],
+        )
+    )
+    target_allocation["Target Allocation (%)"] = target_allocation[
+        "Target Allocation (%)"
+    ].fillna(target_allocation["Client"].map(replacements))
+
+    target_allocation = normalize_column_names(target_allocation)
+    client_profile = normalize_column_names(client_profile)
+
+    return target_allocation, client_profile
+
+
+def normalize_client_allocation(
+    client_allocation: DataFrame,
+) -> tuple[DataFrame, DataFrame]:
+    """
+    Normalize the client allocation DataFrame.
+
+    Args:
+        client_allocation (DataFrame): The client allocation DataFrame.
+
+    Returns:
+        tuple[DataFrame, DataFrame]: A tuple containing the normalized client allocation and
+                                        asset performance DataFrames.
+    """
+
+    client_allocation = fix_client_ids(client_allocation)
+    client_allocation = remove_duplicates(client_allocation)
+    client_allocation = fill_na_values(client_allocation)
+
+    asset_performance = get_asset_performance(client_allocation)
+    client_allocation = get_client_allocation(client_allocation)
+
+    asset_performance = normalize_column_names(asset_performance)
+    client_allocation = normalize_column_names(client_allocation)
+
+    return client_allocation, asset_performance
+
+
 def fix_client_ids(client_allocation: DataFrame) -> DataFrame:
     """
     Fix misspelled client IDs by ensuring they follow the format 'Client_<numeric_id>'.
@@ -38,28 +109,6 @@ def remove_duplicates(client_allocation: DataFrame) -> DataFrame:
         DataFrame: The DataFrame with duplicates removed.
     """
     return client_allocation.drop_duplicates(subset=["Client", "Symbol"])
-
-
-def fill_na_with_reference(
-    df: DataFrame,
-    column_to_replace: str,
-    column_reference: str,
-) -> DataFrame:
-    """
-    Fill NA values in a specified column using a reference column for mapping.
-
-    Args:
-        df (DataFrame): The DataFrame to update.
-        column_to_replace (str): The column in which to fill NA values.
-        column_reference (str): The reference column to use for mapping.
-        reference_df (DataFrame): The DataFrame containing reference data for mapping.
-
-    Returns:
-        DataFrame: The DataFrame with NA values filled.
-    """
-    multi_index = df.groupby(by=[column_to_replace, column_reference]).count().index
-    replacements = {name: symbol for symbol, name in multi_index}
-    return df[column_to_replace].fillna(df[column_reference].map(replacements))
 
 
 def fill_na_values(client_allocation: DataFrame) -> DataFrame:
@@ -112,6 +161,28 @@ def fill_na_values(client_allocation: DataFrame) -> DataFrame:
     return client_allocation
 
 
+def fill_na_with_reference(
+    df: DataFrame,
+    column_to_replace: str,
+    column_reference: str,
+) -> DataFrame:
+    """
+    Fill NA values in a specified column using a reference column for mapping.
+
+    Args:
+        df (DataFrame): The DataFrame to update.
+        column_to_replace (str): The column in which to fill NA values.
+        column_reference (str): The reference column to use for mapping.
+        reference_df (DataFrame): The DataFrame containing reference data for mapping.
+
+    Returns:
+        DataFrame: The DataFrame with NA values filled.
+    """
+    multi_index = df.groupby(by=[column_to_replace, column_reference]).count().index
+    replacements = {name: symbol for symbol, name in multi_index}
+    return df[column_to_replace].fillna(df[column_reference].map(replacements))
+
+
 def get_asset_performance(client_allocation: DataFrame) -> DataFrame:
     """
     Extract columns that relates only with the stocks.
@@ -161,50 +232,6 @@ def get_client_allocation(client_allocation: DataFrame) -> DataFrame:
     return client_allocation
 
 
-def normalize_target_allocation(
-    target_allocation: DataFrame,
-) -> tuple[DataFrame, DataFrame]:
-    """
-    Create target allocation and client profile DataFrames from the target allocation DataFrame.
-
-    Args:
-        target_allocation (DataFrame): The target allocation DataFrame.
-
-    Returns:
-        tuple[DataFrame, DataFrame]: A tuple containing the target allocation DataFrame and the client profile DataFrame.
-    """
-    target_allocation = target_allocation.iloc[:200]
-    client_profile = (
-        target_allocation.groupby(by=["Client", "Target Portfolio"])
-        .count()
-        .reset_index()[["Client", "Target Portfolio"]]
-    )
-
-    target_allocation = target_allocation.drop("Target Portfolio", axis=1)
-    target_allocation["Client"] = [f"Client_{int(i / 4) + 1}" for i in range(50 * 4)]
-    target_allocation["Asset Class"] = ["Stocks", "Bonds", "ETFs", "Cash"] * 50
-
-    allocation_fill_df = (
-        target_allocation.groupby(by="Client")
-        .sum()
-        .reset_index()[["Client", "Target Allocation (%)"]]
-    )
-    replacements = dict(
-        zip(
-            allocation_fill_df["Client"],
-            100 - allocation_fill_df["Target Allocation (%)"],
-        )
-    )
-    target_allocation["Target Allocation (%)"] = target_allocation[
-        "Target Allocation (%)"
-    ].fillna(target_allocation["Client"].map(replacements))
-
-    target_allocation = normalize_column_names(target_allocation)
-    client_profile = normalize_column_names(client_profile)
-
-    return target_allocation, client_profile
-
-
 def normalize_column_names(df: DataFrame) -> DataFrame:
     """
     Normalize the column names
@@ -225,33 +252,6 @@ def normalize_column_names(df: DataFrame) -> DataFrame:
         for c in df
     ]
     return df
-
-
-def normalize_client_allocation(
-    client_allocation: DataFrame,
-) -> tuple[DataFrame, DataFrame]:
-    """
-    Normalize the client allocation DataFrame.
-
-    Args:
-        client_allocation (DataFrame): The client allocation DataFrame.
-
-    Returns:
-        tuple[DataFrame, DataFrame]: A tuple containing the normalized client allocation and
-                                        asset performance DataFrames.
-    """
-
-    client_allocation = fix_client_ids(client_allocation)
-    client_allocation = remove_duplicates(client_allocation)
-    client_allocation = fill_na_values(client_allocation)
-
-    asset_performance = get_asset_performance(client_allocation)
-    client_allocation = get_client_allocation(client_allocation)
-
-    asset_performance = normalize_column_names(asset_performance)
-    client_allocation = normalize_column_names(client_allocation)
-
-    return client_allocation, asset_performance
 
 
 if __name__ == "__main__":
